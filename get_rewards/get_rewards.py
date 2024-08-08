2f316f4ba414c97364ab37d87efa52d23ab9449f
W3i-Stephane <stephane.budo@w3i.io>
2024-08-08 15:25:37 +1000

    Cleanup code

--- get_rewards/get_rewards.py
+++ get_rewards/get_rewards.py
@@ -7,62 +7,65 @@ import json
 import configparser
 from datetime import datetime, timedelta
 
-# Read the configuration file
-config = configparser.ConfigParser()
-configFilePath = r'./get_rewards.conf'
-config.read(configFilePath)
+def read_config(config_file_path):
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    return config
 
-# Set the check date to 2 days ago from now
-check_date = datetime.now()
-check_date -= timedelta(days=2)
+def get_check_date(days_ago=2):
+    return datetime.now() - timedelta(days=days_ago)
 
-# Connect to the SQL database using credentials from the config file
-cnxn = pyodbc.connect(
-    'DRIVER=' + config["sql"]["driver"] + 
-    ';SERVER=tcp:' + config["sql"]["server"] + 
-    ';PORT=1433;DATABASE=' + config["sql"]["database"] + 
-    ';UID=' + config["sql"]["username"] + 
-    ';PWD=' + config["sql"]["password"]
-)
-cursor = cnxn.cursor()
-
-# Create the API request URL with the check date
-url = (
-    "https://api.spacescope.io/v2/economics/block_reward?"
-    "end_date=" + check_date.strftime('%Y-%m-%d') + 
-    "&start_date=" + check_date.strftime('%Y-%m-%d')
-)
-
-# Set the request headers with the API key from the config file
-payload = {}
-headers = {
-    'authorization': 'Bearer ' + config["spacescope"]["apikey"]
-}
-
-# Send a GET request to the API
-response = requests.request("GET", url, headers=headers, data=payload)
-
-# Parse the JSON response
-data = json.loads(response.text)
-
-# Query to check if the reward for the specific date already exists in the database
-query = (
-    "SELECT * FROM BlockRewards WHERE RewardDate='" +
-    str(data['data'][0]['stat_date']) + "'"
-)
-result = cursor.execute(query)
-row = cursor.fetchone()
-
-# If the row doesn't exist, insert the new reward data into the database
-if row is None:
-    insert_query = (
-        "INSERT INTO BlockRewards VALUES('" +
-        str(data['data'][0]['stat_date']) + "'," +
-        str(data['data'][0]['reward_per_wincount']) + ")"
+def connect_to_db(config):
+    return pyodbc.connect(
+        f'DRIVER={config["sql"]["driver"]};'
+        f'SERVER=tcp:{config["sql"]["server"]};'
+        f'PORT=1433;DATABASE={config["sql"]["database"]};'
+        f'UID={config["sql"]["username"]};'
+        f'PWD={config["sql"]["password"]}'
     )
-    cursor.execute(insert_query)
-    cnxn.commit()
 
-# Close the cursor and the database connection
-cursor.close()
-cnxn.close()
+def fetch_block_rewards(check_date, config):
+    url = (
+        "https://api.spacescope.io/v2/economics/block_reward?"
+        f"end_date={check_date.strftime('%Y-%m-%d')}&start_date={check_date.strftime('%Y-%m-%d')}"
+    )
+    headers = {
+        'authorization': f'Bearer {config["spacescope"]["apikey"]}'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def reward_exists(cursor, reward_date):
+    query = "SELECT * FROM BlockRewards WHERE RewardDate=?"
+    cursor.execute(query, reward_date)
+    return cursor.fetchone() is not None
+
+def insert_reward(cursor, reward_date, reward_per_wincount):
+    query = "INSERT INTO BlockRewards (RewardDate, RewardPerWinCount) VALUES (?, ?)"
+    cursor.execute(query, reward_date, reward_per_wincount)
+
+def main():
+    config_file_path = './get_rewards.conf'
+    config = read_config(config_file_path)
+    check_date = get_check_date()
+    
+    cnxn = connect_to_db(config)
+    cursor = cnxn.cursor()
+
+    try:
+        data = fetch_block_rewards(check_date, config)
+        reward_date = data['data'][0]['stat_date']
+        reward_per_wincount = data['data'][0]['reward_per_wincount']
+        
+        if not reward_exists(cursor, reward_date):
+            insert_reward(cursor, reward_date, reward_per_wincount)
+            cnxn.commit()
+        else:
+            print(f"Reward for {reward_date} already exists in the database.")
+    finally:
+        cursor.close()
+        cnxn.close()
+
+if __name__ == "__main__":
+    main()
--- get_transactions/get_transactions.py
+++ get_transactions/get_transactions.py
@@ -4,24 +4,18 @@ import requests
 import pyodbc
 import configparser
 import time
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 import json
 import math
-import sys
 from colorama import Fore
 from requests.exceptions import ChunkedEncodingError, ConnectionError, Timeout
 from json.decoder import JSONDecodeError
 
 # Define constants
-filecoin_genesis_timestamp = 1598306400
-origin = datetime(1970, 1, 1, 0, 0, 0)
-configFilePath = r'./get_transactions.conf'
-page_size = 50
-update_count = 0
-
-# Function to make API calls to Filfox with different call types
-
+FILECOIN_GENESIS_TIMESTAMP = 1598306400
+ORIGIN = datetime(1970, 1, 1, 0, 0, 0)
+CONFIG_FILE_PATH = './get_transactions.conf'
+PAGE_SIZE = 50
 
 def filfox_api_call(call_type, *param):
     headers = {
@@ -31,15 +25,15 @@ def filfox_api_call(call_type, *param):
     payload = {}
 
     if call_type == "get_actor_info":
-        api_request = 'https://filfox.info/api/v1/address/' + str(param[0])
+        api_request = f'https://filfox.info/api/v1/address/{param[0]}'
     elif call_type == "get_tipset_info":
-        api_request = 'https://filfox.info/api/v1/tipset/' + str(param[0])
+        api_request = f'https://filfox.info/api/v1/tipset/{param[0]}'
     elif call_type == "get_all_messages":
-        api_request = 'https://filfox.info/api/v1/address/' + str(param[0]) + '/messages'
+        api_request = f'https://filfox.info/api/v1/address/{param[0]}/messages'
     elif call_type == "get_messages_page":
-        api_request = 'https://filfox.info/api/v1/address/' + str(param[0]) + '/messages?pageSize=' + str(param[2]) + '&page=' + str(param[1])
+        api_request = f'https://filfox.info/api/v1/address/{param[0]}/messages?pageSize={param[2]}&page={param[1]}'
     elif call_type == "get_message":
-        api_request = 'https://filfox.info/api/v1/message/' + str(param[0])
+        api_request = f'https://filfox.info/api/v1/message/{param[0]}'
 
     retries = 5
     backoff_factor = 1
@@ -47,10 +41,10 @@ def filfox_api_call(call_type, *param):
 
     for attempt in range(retries):
         try:
-            response = requests.request("GET", api_request, headers=headers, data=payload, timeout=timeout)
+            response = requests.get(api_request, headers=headers, data=payload, timeout=timeout)
             response.raise_for_status()
             try:
-                return json.loads(response.text)
+                return response.json()
             except JSONDecodeError as e:
                 print(f"JSON decode error: {e}")
                 if attempt < retries - 1:
@@ -72,138 +66,133 @@ def filfox_api_call(call_type, *param):
         except requests.exceptions.HTTPError as err:
             print(f"HTTP error occurred: {err}")
             if response.status_code != 200:
-                print("Filfox API error:" + str(response.status_code))
-                print("will try again in 20 seconds")
+                print(f"Filfox API error: {response.status_code}")
+                print("Will try again in 20 seconds")
                 time.sleep(20)
             else:
                 return None
         except Exception as e:
             print(f"Unexpected error occurred: {e}")
             return None
-        
 
-print(datetime.now(), "- Starting...")
+def main():
+    print(datetime.now(), "- Starting...")
+    print(datetime.now(), "- Reading configuration file")
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE_PATH)
 
-print(datetime.now(), "- Reading configuration file")
-# Read the config file
-config = configparser.ConfigParser()
+    cnxn = pyodbc.connect(
+        f'DRIVER={config["sql"]["driver"]};'
+        f'SERVER=tcp:{config["sql"]["server"]};'
+        f'PORT=1433;DATABASE={config["sql"]["database"]};'
+        f'UID={config["sql"]["username"]};'
+        f'PWD={config["sql"]["password"]}'
+    )
+    cursor = cnxn.cursor()
 
-config.read(configFilePath)
+    query = "SELECT * FROM AccountsList WHERE Active=1"
+    cursor.execute(query)
+    accounts_list = cursor.fetchall()
 
+    for account in accounts_list:
+        print(Fore.GREEN + f"New account: {account.ActorID}")
 
+        if account.IsMiner:
+            print(Fore.YELLOW + 'Account is a miner, checking for block rewards')
+            miner_info = filfox_api_call("get_actor_info", account.ActorID)
+            last_seen_height = miner_info["lastSeenHeight"]
+            last_epoch_check_rewards = account.LastEpochCheckRewards
+            if last_seen_height > last_epoch_check_rewards:
+                while last_epoch_check_rewards < last_seen_height:
+                    epochs = filfox_api_call("get_tipset_info", last_epoch_check_rewards)
+                    print(Fore.WHITE + str(last_epoch_check_rewards))
+                    for reward in epochs["blocks"]:
+                        if reward["miner"] == account.ActorID:
+                            print(Fore.CYAN + f"Block reward found at height: {last_epoch_check_rewards}")
+                            timestamp = ORIGIN + timedelta(seconds=(FILECOIN_GENESIS_TIMESTAMP + (last_epoch_check_rewards * 30)))
+                            query = (
+                                "INSERT INTO FilecoinTransactionsList "
+                                "(MessageID, MessageDate, ActorID, Height, Nonce, MethodID, MethodName, ExitCode, TransferType, TransferFrom, TransferTo, TransferAmount) "
+                                "VALUES "
+                                f"('{reward['cid']}', '{timestamp}', '{reward['miner']}', {last_epoch_check_rewards}, 0, 14, 'AddLockedFund', 0, 'reward', 'f02', '{reward['miner']}', {reward['reward']})"
+                            )
+                            cursor.execute(query)
+                            cnxn.commit()
+                    last_epoch_check_rewards += 1
+                    update_query = f"UPDATE AccountsList SET LastEpochCheckRewards={last_epoch_check_rewards} WHERE ActorID='{account.ActorID}'"
+                    cursor.execute(update_query)
+                    cnxn.commit()
 
-# Connect to the SQL database
-cnxn = pyodbc.connect(
-    'DRIVER=' + config["sql"]["driver"] +
-    ';SERVER=tcp:' + config["sql"]["server"] +
-    ';PORT=1433;DATABASE=' + config["sql"]["database"] +
-    ';UID=' + config["sql"]["username"] +
-    ';PWD=' + config["sql"]["password"]
-)
-cursor = cnxn.cursor()
-
-# Fetch the list of active accounts from the database
-query = "select * from AccountsList where Active=1"
-cursor.execute(query)
-accounts_list = cursor.fetchall()
-
-
-
-
-# Loop through each account in the accounts list
-for account in accounts_list:
-    print(Fore.GREEN + "new account: " + account.ActorID)
-
-    # If the account is a miner, check for block rewards
-    if account.IsMiner == True:
-        print(Fore.YELLOW + 'account is a miner, checking for block rewards')
-        miner_info = filfox_api_call("get_actor_info", account.ActorID)
-        last_seen_height = miner_info["lastSeenHeight"]
-        last_epoch_check_rewards = account.LastEpochCheckRewards
-        if last_seen_height > last_epoch_check_rewards:
-            while last_epoch_check_rewards < last_seen_height:
-                epochs = filfox_api_call("get_tipset_info", last_epoch_check_rewards)
-                print(Fore.WHITE + str(last_epoch_check_rewards))
-                for reward in epochs["blocks"]:
-                    if reward["miner"] == account.ActorID:
-                        print(Fore.CYAN + "block reward found at height: " + str(last_epoch_check_rewards))
-                        timestamp = origin + timedelta(seconds=(filecoin_genesis_timestamp + (last_epoch_check_rewards * 30)))
-                        query = (
-                            "INSERT INTO FilecoinTransactionsList (MessageID,MessageDate,ActorID,Height,Nonce,MethodID,MethodName,ExitCode,TransferType,TransferFrom,TransferTo,TransferAmount) VALUES "
-                            "('" + str(reward["cid"]) + "','" + str(timestamp) + "','" + str(reward["miner"]) + "'," + str(last_epoch_check_rewards) + ",0,14,'AddLockedFund'"
-                            ",0,'reward','f02','" + str(reward["miner"]) + "'," + str(reward["reward"]) + ")"
-                        )
-                        cursor.execute(query)
-                        cnxn.commit()
-                last_epoch_check_rewards += 1
-                update_query = "Update AccountsList set LastEpochCheckRewards=" + str(last_epoch_check_rewards) + " where ActorID='" + account.ActorID + "'"
-                cursor.execute(update_query)
-                cnxn.commit()
-
-    # Fetch all messages for the account
-    messages = filfox_api_call("get_all_messages", account.ActorID)
-    print(Fore.WHITE + "checking for new messages")
-    if account.CheckAllMessages == 1:
-        num_processed_pages = 0
-        counter = 0
-    else:
-        counter = account.Counter
-        num_processed_pages = math.floor(counter / page_size)
-    print(Fore.WHITE + "Current counter value in DB:" + str(counter))
-    print(Fore.WHITE + "Messages total count in filfox:" + str(messages["totalCount"]))
-    if counter < messages["totalCount"]:
-        print(Fore.WHITE + "Messages found, processing...")
-        total_num_pages = math.ceil(messages["totalCount"] / page_size)
-        num_pages = range(total_num_pages - num_processed_pages)
-        print(Fore.WHITE + "total number of pages in filfox: " + str(total_num_pages))
-        print(Fore.WHITE + "total already processed pages: " + str(num_processed_pages))
-        print(Fore.WHITE + "Number of pages to be processed: " + str(num_pages))
-        # Process each page of messages
-        for page_number in num_pages:
-            print(Fore.MAGENTA + "processing page: " + str(num_processed_pages + page_number) + "/" + str(total_num_pages) + " (Account: " + account.ActorID + ")")
-            messages = filfox_api_call("get_messages_page", account.ActorID, str(page_number), page_size)
-            for message in messages["messages"]:
-                query = "select * from FilecoinTransactionsList where MessageID='" + message["cid"] + "'"
-                cursor.execute(query)
-                check_message_exists = cursor.fetchone()
-                if check_message_exists is None:
-                    print(Fore.YELLOW + "adding message to database:" + account.ActorID + " - " + message["cid"])
-                    transaction = filfox_api_call("get_message", message["cid"])
-                    if transaction is not None:
-                        if "transfers" in transaction:
-                            for transfer in transaction["transfers"]:
-                                if transfer["fromId"] == account.ActorID:
-                                    timestamp = origin + timedelta(seconds=transaction["timestamp"])
-                                    insert_query = (
-                                        "INSERT INTO FilecoinTransactionsList (MessageID,MessageDate,ActorID,Height,Nonce,MethodID,MethodName,ExitCode,TransferType,TransferFrom,TransferTo,TransferAmount) VALUES "
-                                        "('" + str(transaction["cid"]) + "','" + str(timestamp) + "','" + str(account.ActorID) + "'," + str(transaction["height"]) + "," + str(transaction["nonce"]) + "," + str(transaction["methodNumber"]) + ",'" + str(transaction["method"]) + "'"
-                                        "," + str(transaction["receipt"]["exitCode"]) + ",'" + str(transfer["type"]) + "','" + str(transfer["fromId"]) + "','" + str(transfer["toId"]) + "',-" + str(transfer["value"]) + ")"
-                                    )
-                                    cursor.execute(insert_query)
-                                    cnxn.commit()
-                                    print(Fore.LIGHTBLUE_EX + "adding transaction to database:" + account.ActorID + " - " + transaction["cid"])
-                                else:
-                                    if transfer["type"] == "transfer":
-                                        timestamp = origin + timedelta(seconds=transaction["timestamp"])
+        messages = filfox_api_call("get_all_messages", account.ActorID)
+        print(Fore.WHITE + "Checking for new messages")
+        if account.CheckAllMessages:
+            num_processed_pages = 0
+            counter = 0
+        else:
+            counter = account.Counter
+            num_processed_pages = math.floor(counter / PAGE_SIZE)
+        print(Fore.WHITE + f"Current counter value in DB: {counter}")
+        print(Fore.WHITE + f"Messages total count in filfox: {messages['totalCount']}")
+        if counter < messages["totalCount"]:
+            print(Fore.WHITE + "Messages found, processing...")
+            total_num_pages = math.ceil(messages["totalCount"] / PAGE_SIZE)
+            num_pages = range(total_num_pages - num_processed_pages)
+            print(Fore.WHITE + f"Total number of pages in filfox: {total_num_pages}")
+            print(Fore.WHITE + f"Total already processed pages: {num_processed_pages}")
+            print(Fore.WHITE + f"Number of pages to be processed: {list(num_pages)}")
+            for page_number in num_pages:
+                print(Fore.MAGENTA + f"Processing page: {num_processed_pages + page_number}/{total_num_pages} (Account: {account.ActorID})")
+                messages_page = filfox_api_call("get_messages_page", account.ActorID, page_number, PAGE_SIZE)
+                if messages_page is None:
+                    print(Fore.RED + "Error fetching messages page")
+                    continue
+                for message in messages_page["messages"]:
+                    query = f"SELECT * FROM FilecoinTransactionsList WHERE MessageID='{message['cid']}'"
+                    cursor.execute(query)
+                    check_message_exists = cursor.fetchone()
+                    if check_message_exists is None:
+                        print(Fore.YELLOW + f"Adding message to database: {account.ActorID} - {message['cid']}")
+                        transaction = filfox_api_call("get_message", message["cid"])
+                        if transaction is not None:
+                            if "transfers" in transaction:
+                                for transfer in transaction["transfers"]:
+                                    if transfer["fromId"] == account.ActorID:
+                                        timestamp = ORIGIN + timedelta(seconds=transaction["timestamp"])
                                         insert_query = (
-                                            "INSERT INTO FilecoinTransactionsList (MessageID,MessageDate,ActorID,Height,Nonce,MethodID,MethodName,ExitCode,TransferType,TransferFrom,TransferTo,TransferAmount) VALUES "
-                                            "('" + str(transaction["cid"]) + "','" + str(timestamp) + "','" + str(account.ActorID) + "'," + str(transaction["height"]) + "," + str(transaction["nonce"]) + "," + str(transaction["methodNumber"]) + ",'" + str(transaction["method"]) + "'"
-                                            "," + str(transaction["receipt"]["exitCode"]) + ",'" + str(transfer["type"]) + "','" + str(transfer["fromId"]) + "','" + str(transfer["toId"]) + "'," + str(transfer["value"]) + ")"
+                                            "INSERT INTO FilecoinTransactionsList "
+                                            "(MessageID, MessageDate, ActorID, Height, Nonce, MethodID, MethodName, ExitCode, TransferType, TransferFrom, TransferTo, TransferAmount) "
+                                            "VALUES "
+                                            f"('{transaction['cid']}', '{timestamp}', '{account.ActorID}', {transaction['height']}, {transaction['nonce']}, {transaction['methodNumber']}, '{transaction['method']}', "
+                                            f"{transaction['receipt']['exitCode']}, '{transfer['type']}', '{transfer['fromId']}', '{transfer['toId']}', -{transfer['value']})"
                                         )
                                         cursor.execute(insert_query)
                                         cnxn.commit()
-                                        print(Fore.LIGHTBLUE_EX + "adding transaction to database:" + account.ActorID + " - " + transaction["cid"])
-                    counter += 1
-                    update_query = "Update AccountsList set Counter=" + str(counter) + " where ActorID='" + str(account.ActorID) + "'"
-                    cursor.execute(update_query)
-                    cnxn.commit()
-                else:
-                    print(Fore.WHITE + "Message already exists: " + message["cid"])
-                    if account.CheckAllMessages == 1:
+                                        print(Fore.LIGHTBLUE_EX + f"Adding transaction to database: {account.ActorID} - {transaction['cid']}")
+                                    elif transfer["type"] == "transfer":
+                                        timestamp = ORIGIN + timedelta(seconds=transaction["timestamp"])
+                                        insert_query = (
+                                            "INSERT INTO FilecoinTransactionsList "
+                                            "(MessageID, MessageDate, ActorID, Height, Nonce, MethodID, MethodName, ExitCode, TransferType, TransferFrom, TransferTo, TransferAmount) "
+                                            "VALUES "
+                                            f"('{transaction['cid']}', '{timestamp}', '{account.ActorID}', {transaction['height']}, {transaction['nonce']}, {transaction['methodNumber']}, '{transaction['method']}', "
+                                            f"{transaction['receipt']['exitCode']}, '{transfer['type']}', '{transfer['fromId']}', '{transfer['toId']}', {transfer['value']})"
+                                        )
+                                        cursor.execute(insert_query)
+                                        cnxn.commit()
+                                        print(Fore.LIGHTBLUE_EX + f"Adding transaction to database: {account.ActorID} - {transaction['cid']}")
                         counter += 1
-                        update_query = "Update AccountsList set Counter=" + str(counter) + " where ActorID='" + str(account.ActorID) + "'"
+                        update_query = f"UPDATE AccountsList SET Counter={counter} WHERE ActorID='{account.ActorID}'"
                         cursor.execute(update_query)
                         cnxn.commit()
-        # time.sleep(0.2)
-    else:
-        time.sleep(1)
+                    else:
+                        print(Fore.WHITE + f"Message already exists: {message['cid']}")
+                        if account.CheckAllMessages:
+                            counter += 1
+                            update_query = f"UPDATE AccountsList SET Counter={counter} WHERE ActorID='{account.ActorID}'"
+                            cursor.execute(update_query)
+                            cnxn.commit()
+        else:
+            time.sleep(1)
+
+if __name__ == "__main__":
+    main()
